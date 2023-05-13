ace34e155b6cca47433dfdf24540b2492429b179
unknown <o_amine@etu.enset-media.ac.ma>
2023-05-13 23:23:40 +0100

    https://github.com/Omayma-AMINE/tpHospital.git

--- src/main/java/ma/enset/tphospital/entities/Patient.java
+++ src/main/java/ma/enset/tphospital/entities/Patient.java
@@ -1,6 +1,9 @@
 package ma.enset.tphospital.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,10 +15,13 @@ import java.util.Date;
 public class Patient {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id ;
+    @NotEmpty
+    @Size(min=4, max=20)
     private String nom;
     @Temporal(TemporalType.DATE)
     private Date dateNaissance;
     private boolean malade ;
+    @Min(10)
     private int score ;
 
 
--- src/main/java/ma/enset/tphospital/web/PatientConroller.java
+++ src/main/java/ma/enset/tphospital/web/PatientConroller.java
@@ -1,5 +1,6 @@
 package ma.enset.tphospital.web;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import ma.enset.tphospital.entities.Patient;
 import ma.enset.tphospital.repository.PatientRepository;
@@ -8,7 +9,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -36,6 +39,24 @@ public class PatientConroller {
         patientRepository.deleteById(id);
         return "redirect:/index?page="+page+"&keyword="+keyword;
     }
-
+    @GetMapping("/formPatient")
+    public String formPatient(Model model){
+        model.addAttribute("patient",new Patient());
+        return "formPatient";
+    }
+    @PostMapping("/savePatient")
+    public String savePatient(@Valid Patient patient,
+                              BindingResult bindingResult){
+        if(bindingResult.hasErrors())
+            return "formPatient";
+        patientRepository.save(patient);
+        return "redirect:/index?keyword="+patient.getNom();
+    }
+    @GetMapping("/editPatient")
+    public  String editPatient(Model model, @RequestParam(name="id") Long id){
+        Patient patient = patientRepository.findById(id).get();
+        model.addAttribute("patient",patient);
+        return "editPatient";
+    }
 
 }
